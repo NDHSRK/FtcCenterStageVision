372dc54ac79c40759584d0b314bb2a8ad6d0c0ca
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2024-02-06 09:32:54 -0800

    WIP: custom changes to SetStartParameters2 for Qualia (3)

--- ftc-center-stage-vision/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/configure/SetStartParameters2.java
+++ ftc-center-stage-vision/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/configure/SetStartParameters2.java
@@ -30,18 +30,9 @@ public class SetStartParameters2 extends LinearOpMode {
 
     private StartParametersXML startParametersXML;
     private StartParameters startParameters;
+    private boolean startParametersXMLChanged = false;
 
-    private static final int MAX_DELAY = 10;
-    private int currentStartDelay;
-    private FTCButton increaseStartDelay;
-    private FTCButton decreaseStartDelay;
-
-    EnumMap<RobotConstantsCenterStage.OpMode, RobotConstantsCenterStage.AutoEndingPosition> autoEndingPositions;
-    RobotConstantsCenterStage.OpMode currentOpMode = RobotConstantsCenterStage.OpMode.OPMODE_NPOS;
-    private boolean endPositionsChanged = false;
-    private FTCButton endPositionLeft;
-    private FTCButton endPositionRight;
-    private FTCButton factoryReset; // GAMEPAD_1_START
+    private static final int MAX_DELAY = 10; // applies to all delays
 
     // DPAD left toggle button position A (default) is the standard button layout.
     // DPAD left Toggle button position B is the Qualia layout.
@@ -50,43 +41,56 @@ public class SetStartParameters2 extends LinearOpMode {
     private Mode mode = Mode.STANDARD;
     private FTCToggleButton toggleMode;
 
-    private FTCButton modalButton1A; // standard opModeBlueA2, Qualia wall_truss
-    private FTCButton modalButton1X; // standard opModeBlueA4, QWualia center_truss path
-    private FTCButton modalButton1Y; // standard opModeRedF4, Qualia stage_door path
-    private FTCButton modalButton1B; // standard opModeRedF2
+    // This section applies to the STANDARD model only
+    private int currentStartDelay;
+    RobotConstantsCenterStage.OpMode currentOpMode = RobotConstantsCenterStage.OpMode.OPMODE_NPOS;
+    EnumMap<RobotConstantsCenterStage.OpMode, RobotConstantsCenterStage.AutoEndingPosition> autoEndingPositions;
+    private FTCButton endPositionLeft;
+    private FTCButton endPositionRight;
+    private FTCButton factoryReset;
+    // End STANDARD mode section
 
-    // Qualia only
+    // This section applies to the QUALIA mode only.
+    private StartParameters.QualiaStartParameters.Path currentPath;
+    private int currentPostSpikeDelay;
+    private int currentPreBackstageDelay;
     private FTCToggleButton midPointDelay;
+    // End QUALIA mode section
+
+    // This section applies to the modal use of same buttons
+    private FTCButton modalIncreaseDelay; // standard increase start delay, Qualia increase mid-point delay
+    private FTCButton modalDecreaseDelay; // standard decrease start delay, Qualia decrease mid-point delay
+    private FTCButton modalButton1A; // standard opModeBlueA2, Qualia wall_truss
+    private FTCButton modalButton1X; // standard opModeBlueA4, Qualia center_truss path
+    private FTCButton modalButton1Y; // standard opModeRedF4, Qualia stage_door path
+    private FTCButton modalButton1B; // standard opModeRedF2, no Qualia use
+    // End modal section
 
     @Override
     public void runOpMode() {
-        //**TODO increase/decrease buttons must be modal also - in the STANDARD mode
-        // they determine the start delay; in QUALIA mode they determine the mid point
-        // delay(s). At least change the names to increaseDelay and decreaseDelay or
-        // modalIncreaseDelay ...
-        increaseStartDelay = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_UP);
-        decreaseStartDelay = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_DOWN);
-
-        endPositionLeft = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_LEFT);
-        endPositionRight = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_RIGHT);
-        factoryReset = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_START);
-
         // Toggle button for switching between standard mode and Qualia mode.
         // DPAD left toggle button position A (default) is the standard button layout.
         // DPAD left Toggle button position B is the Qualia layout.
         toggleMode = new FTCToggleButton(this, FTCButton.ButtonValue.GAMEPAD_1_LEFT_BUMPER);
 
-        // The meaning of these buttons depends upon the current mode.
-        modalButton1A = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_A);
-        modalButton1X = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_X);
-        modalButton1Y = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_Y);
-        modalButton1B = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_B);
+        // Button assignments for STANDARD mode.
+        endPositionLeft = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_LEFT);
+        endPositionRight = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_RIGHT);
+        factoryReset = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_START);
 
-        // Qualia only
+        // Button assignments for QUALIA mode.
         // Toggle button for switching between a post_spike delay and a pre_backstage delay.
         // DPAD right toggle button position A (default) is for the post_spike delay.
         // DPAD right Toggle button position B is for the pre_backstage delay.
         midPointDelay = new FTCToggleButton(this, FTCButton.ButtonValue.GAMEPAD_1_RIGHT_BUMPER);
+
+        // Modal button assignments.
+        modalIncreaseDelay = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_UP);
+        modalDecreaseDelay = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_DPAD_DOWN);
+        modalButton1A = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_A);
+        modalButton1X = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_X);
+        modalButton1Y = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_Y);
+        modalButton1B = new FTCButton(this, FTCButton.ButtonValue.GAMEPAD_1_B);
 
         String fullXMLDir = WorkingDirectory.getWorkingDirectory() + RobotConstants.XML_DIR;
         try {
@@ -95,14 +99,22 @@ public class SetStartParameters2 extends LinearOpMode {
             throw new RuntimeException(e);
         }
 
+        startParameters = startParametersXML.getStartParameters();
+
+        currentStartDelay = startParameters.autoStartDelay;
+        autoEndingPositions = new EnumMap<>(startParameters.autoEndingPositions); // copy
+
         //## Note: StartParameters.QualiaStartParameters may be null because
         // these parameters are optional. If this object is null then we will
         // disable the toggle button and all of the Qualia actions.
-        startParameters = startParametersXML.getStartParameters();
+        if (startParameters.qualiaStartParameters != null) {
+            currentPath = startParameters.qualiaStartParameters.path;
+            currentPostSpikeDelay = startParameters.qualiaStartParameters.midPathDelayPostSpike;
+            currentPreBackstageDelay = startParameters.qualiaStartParameters.midPathDelayPreBackstage;
+        }
 
-        int startDelay;
-        currentStartDelay = startDelay = startParameters.autoStartDelay;
-
+        //**TODO this should be part of the main loop ... with different
+        // output depending on the mode.
         telemetry.addLine("The current mode is STANDARD");
         if (startParameters.qualiaStartParameters != null)
             telemetry.addLine("Press the DPAD left bumper to toggle to QUALIA");
@@ -116,44 +128,33 @@ public class SetStartParameters2 extends LinearOpMode {
         telemetry.addLine("Touch play to SAVE changes and END the OpMode");
         telemetry.update();
 
-        autoEndingPositions = new EnumMap<>(startParameters.autoEndingPositions); // copy
-
         while (!isStarted() && !isStopRequested()) {
             updateButtons();
             updatePlayer1();
 
-            // Don't burn CPU cycles busy-looping in this sample
-            sleep(50);
+            //**TODO updateTelemetry();
+
+            sleep(50); // Don't burn CPU cycles busy-looping
         } // while
 
         if (opModeIsActive()) {
-            //**TODO Detect Qualia changes also ...
-            if (startDelay != currentStartDelay || endPositionsChanged) {
-                if (startDelay != currentStartDelay) {
-                    startParametersXML.setAutoStartDelay(currentStartDelay);
-                    RobotLog.ii(TAG, "Changed start delay to " + currentStartDelay);
-                    telemetry.addLine("Changed start delay to " + currentStartDelay);
-                }
-
-                if (endPositionsChanged) {
-                    RobotLog.ii(TAG, "Changed one or more ending positions");
-                    telemetry.addLine("Changed one or more ending positions");
-                }
-
+            if (startParametersXMLChanged) {
                 startParametersXML.writeStartParametersFile();
                 RobotLog.ii(TAG, "Writing StartParameters.xml");
                 telemetry.addLine("Writing StartParameters.xml");
-                telemetry.update();
-                sleep(1500);
-            }
+            } else
+                telemetry.addLine("No changes to StartParameters.xml");
+
+            telemetry.update();
+            sleep(1500);
         }
     }
 
     private void updateButtons() {
         toggleMode.update();
 
-        increaseStartDelay.update();
-        decreaseStartDelay.update();
+        modalIncreaseDelay.update();
+        modalDecreaseDelay.update();
         endPositionLeft.update();
         endPositionRight.update();
         factoryReset.update();
@@ -176,15 +177,20 @@ public class SetStartParameters2 extends LinearOpMode {
             updateOpModeRedF4();
             updateEndPositionLeft();
             updateEndPositionRight();
-            //**TODO updateFactoryReset();
+            updateFactoryReset();
         } else {
             //**TODO call Qualia update methods
             // updateWallTrussPath();
             // updateCenterTrussPath();
             // updateStageDoorPath();
+            // updateDelayPointMode();
+            // updateIncreasePostSpikeDelay();
+            // updateDecreasePostSpikeDelay();
+            // updateIncreasePreBackstageDelay();
+            // updateDecreasePreBackstageDelay();
             /*
                private FTCButton modalButton1A; // standard opModeBlueA2, Qualia wall_truss
-    private FTCButton modalButton1X; // standard opModeBlueA4, QWualia center_truss path
+    private FTCButton modalButton1X; // standard opModeBlueA4, Qualia center_truss path
     private FTCButton modalButton1Y; // standard opModeRedF4, Qualia stage_door path
              */
         }
@@ -204,24 +210,31 @@ public class SetStartParameters2 extends LinearOpMode {
         }
     }
 
+    // STANDARD mode methods.
     private void updateIncreaseStartDelay() {
-        if (increaseStartDelay.is((FTCButton.State.TAP))) {
-            currentStartDelay = currentStartDelay < MAX_DELAY ? ++currentStartDelay : MAX_DELAY;
+        if (modalIncreaseDelay.is((FTCButton.State.TAP))) {
             if (currentStartDelay == MAX_DELAY)
                 telemetry.addLine("Start delay is at the maximum of " + MAX_DELAY);
-            else
-                telemetry.addLine("Start delay increased to " + currentStartDelay);
+            else {
+                ++currentStartDelay;
+                startParametersXML.setAutoStartDelay(currentStartDelay);
+                startParametersXMLChanged = true;
+            }
+
             telemetry.update();
         }
     }
 
     private void updateDecreaseStartDelay() {
-        if (decreaseStartDelay.is((FTCButton.State.TAP))) {
-            currentStartDelay = currentStartDelay >= 1 ? --currentStartDelay : 0; // 0 or positive
+        if (modalDecreaseDelay.is((FTCButton.State.TAP))) {
             if (currentStartDelay == 0)
                 telemetry.addLine("Start delay is at the minimum of 0");
-            else
-                telemetry.addLine("Start delay decreased to " + currentStartDelay);
+            else {
+                --currentStartDelay;
+                startParametersXML.setAutoStartDelay(currentStartDelay);
+                startParametersXMLChanged = true;
+            }
+
             telemetry.update();
         }
     }
@@ -261,7 +274,7 @@ public class SetStartParameters2 extends LinearOpMode {
                 return;
             }
 
-            endPositionsChanged = true;
+            startParametersXMLChanged = true;
             startParametersXML.setAutoEndingPosition(currentOpMode, RobotConstantsCenterStage.AutoEndingPosition.LEFT.toString());
             autoEndingPositions.put(currentOpMode, RobotConstantsCenterStage.AutoEndingPosition.LEFT);
 
@@ -285,7 +298,7 @@ public class SetStartParameters2 extends LinearOpMode {
                 return;
             }
 
-            endPositionsChanged = true;
+            startParametersXMLChanged = true;
             startParametersXML.setAutoEndingPosition(currentOpMode, RobotConstantsCenterStage.AutoEndingPosition.RIGHT.toString());
             autoEndingPositions.put(currentOpMode, RobotConstantsCenterStage.AutoEndingPosition.RIGHT);
 
@@ -294,4 +307,71 @@ public class SetStartParameters2 extends LinearOpMode {
         }
     }
 
+    private void updateFactoryReset() {
+        if (factoryReset.is(FTCButton.State.TAP)) {
+            //**TODO re-read the XML file and reset all fields and flags.
+        }
+    };
+
+    // QUALIA mode methods.
+
+    //                private FTCButton modalButton1A; // standard opModeBlueA2, Qualia wall_truss
+    // private void updateWallTrussPath() {
+    //        if (modalButton1A.is(FTCButton.State.TAP)) {
+    //            //**TODO re-read the XML file and reset all fields and flags.
+    //        }
+    //    };
+
+    //     private FTCButton modalButton1X; // standard opModeBlueA4, Qualia center_truss path
+    // private void updateCenterTrussPath() {
+    //        if (modalButton1X.is(FTCButton.State.TAP)) {
+    //            //**TODO re-read the XML file and reset all fields and flags.
+    //        }
+    //    };
+
+    //    private FTCButton modalButton1Y; // standard opModeRedF4, Qualia stage_door path
+    // private void updateStageDoorPath() {
+    //        if (modalButton1Y.is(FTCButton.State.TAP)) {
+    //            //**TODO re-read the XML file and reset all fields and flags.
+    //        }
+    //    };
+
+   /*
+       private void updateDelayPointMode() { //**TODO
+        if (toggleMode.is(FTCButton.State.TAP)) {
+            if (toggleMode.toggle() == FTCToggleButton.ToggleState.A)
+                mode = Mode.STANDARD;
+            else {
+                if (startParameters.qualiaStartParameters == null) {
+                    telemetry.addLine("Toggle disallowed: Qualia elements are not present in StartParameters.xml");
+                    telemetry.update();
+                } else
+                    mode = Mode.QUALIA;
+            }
+        }
+    }
+    */
+    // private void updateIncreasePostSpikeDelay() {
+    //        if (modalIncreaseDelay.is(FTCButton.State.TAP)) {
+    //            //**TODO re-read the XML file and reset all fields and flags.
+    //        }
+    //    };
+
+    // private void updateDecreasePostSpikeDelay() {
+    //        if (modalDecreaseDelay.is(FTCButton.State.TAP)) {
+    //            //**TODO re-read the XML file and reset all fields and flags.
+    //        }
+    //    };
+
+    // private void updateIncreasePreBackstageDelay() {
+    //        if (modalIncreaseDelay.is(FTCButton.State.TAP)) {
+    //            //**TODO re-read the XML file and reset all fields and flags.
+    //        }
+    //    };
+
+    // private void updateDecreasePreBackstageDelay() {
+    //        if (modalDecreaseDelay.is(FTCButton.State.TAP)) {
+    //            //**TODO re-read the XML file and reset all fields and flags.
+    //        }
+    //    };
 }
