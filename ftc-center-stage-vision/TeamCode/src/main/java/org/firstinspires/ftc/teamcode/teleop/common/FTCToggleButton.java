4d93249cbea75a109443390ec04904571c3780ff
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2024-02-09 17:18:06 -0800

    Minor change to FTCToggleButton to match FtcCenterStage

--- ftc-center-stage-vision/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/common/FTCToggleButton.java
+++ ftc-center-stage-vision/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/common/FTCToggleButton.java
@@ -5,8 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 // Simple A/B toggle that starts in position A.
 public class FTCToggleButton extends FTCButton {
 
-    private static final String TAG = FTCToggleButton.class.getSimpleName();
-
     public enum ToggleState {A, B}
 
     private ToggleState toggleState = ToggleState.A;
@@ -16,7 +14,7 @@ public class FTCToggleButton extends FTCButton {
     }
 
     public ToggleState toggle() {
-        toggleState = (toggleState == ToggleState.A) ? ToggleState.B : toggleState.A;
+        toggleState = (toggleState == ToggleState.A) ? ToggleState.B : ToggleState.A;
         return toggleState;
     }
 
